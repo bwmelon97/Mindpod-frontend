7006b0d70e5c98f3a5d0a13ae084016abfe1966f
Soogeun Park <bwmelon97@naver.com>
2021-05-25 08:29:43 +0000

    login page: styling page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,8 +3,25 @@ import React from "react";
 function LoginPage () {
 
     return (
-        <div>
-            Login Page
+        <div className='bg-gray-200 flex justify-center h-screen items-center'>
+            <div className='bg-white w-full max-w-xl flex flex-col items-center py-16 px-10 shadow-xl rounded-xl'>
+                <h3 className='mb-10 text-2xl font-light select-none' > Login </h3>
+                <form>
+                    <input 
+                        className='mb-6 w-full px-5 py-3 rounded-md ring-2 ring-gray-200 focus:ring-2 focus:ring-indigo-400 focus:outline-none'
+                        placeholder='Email'
+                    />
+                    <input 
+                        className='mb-6 w-full px-5 py-3 rounded-md ring-2 ring-gray-200 focus:ring-2 focus:ring-indigo-400 focus:outline-none'
+                        placeholder='Password'
+                    />
+                    <button
+                        className='w-full px-5 py-3 rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 text-white focus:outline-none'
+                    > 
+                        Login 
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
