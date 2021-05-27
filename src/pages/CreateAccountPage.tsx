a65004e4a4f67e89aaaa32e017d5a99aff104782
Soogeun Park <bwmelon97@naver.com>
2021-05-27 06:37:52 +0000

    CreateAccountPage: implement create account mutation

--- src/pages/CreateAccountPage.tsx
+++ src/pages/CreateAccountPage.tsx
@@ -1,19 +1,53 @@
 import React from "react";
+import gql from "graphql-tag";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { UserRole } from "../__generated__/globalTypes";
+import { CreateAccount, CreateAccountVariables } from "../__generated__/CreateAccount";
 
+
+const CREATE_ACCOUNT_MUTATION = gql`
+    mutation CreateAccount( $createAccountInput: CreateAccountInput! ) {
+        createAccount(input: $createAccountInput) {
+            ok
+            error
+        }
+    }
+`;
 
 type CreateAccountFormInput = {
     email: string;
     password: string;
+    role: UserRole;
 }
 
 function CreateAccountPage () {
 
     const { register, handleSubmit, getValues, formState: { errors } } = useForm<CreateAccountFormInput>();
 
-    const onSubmit = () => { console.log(getValues()) }
-    const loading = false
+    const onCompleted = ({ createAccount }: CreateAccount) => {
+        const { ok } = createAccount;
+        if ( ok ) {
+            console.log('Success to Create Account')
+        }
+    }
+    const [ 
+        createAccountMutation, { loading, data: createAccountData } 
+    ] = useMutation<CreateAccount, CreateAccountVariables>(CREATE_ACCOUNT_MUTATION, { onCompleted })
+
+    const onSubmit = () => { 
+        if (!loading) {
+            const { email, password, role } = getValues();
+            createAccountMutation({
+                variables: {
+                    createAccountInput: {
+                        email, password, role
+                    }
+                }
+            })
+        }
+    }
 
     return (
         <div className='bg-gray-200 flex justify-center h-screen items-center'>
@@ -37,11 +71,23 @@ function CreateAccountPage () {
                         placeholder='Password'
                     />
                     <select
+                        {...register('role', {
+                            required: 'Role is required.'
+                        })}
                         className='w-full px-5 py-3 rounded-md ring-2 ring-gray-200 focus:ring-2 focus:ring-indigo-400 focus:outline-none'
                     >
+                        { Object.keys(UserRole).map( (role, idx) => {
+                            return (
+                                <option key={idx}>
+                                    {role}
+                                </option>
+                            )
+                        }) }
                     </select>
                     { errors.email?.message && <p className='text-red-500'> {errors.email.message} </p> }
                     { errors.password?.message && <p className='text-red-500'> {errors.password.message} </p> }
+                    { errors.role?.message && <p className='text-red-500'> {errors.role.message} </p> }
+                    { createAccountData?.createAccount.error && <p className='text-red-500'> {createAccountData?.createAccount.error} </p> }
                     <button
                         className='w-full px-5 py-3 rounded-md bg-gradient-to-r from-purple-600 to-indigo-600 text-white focus:outline-none'
                     > 
