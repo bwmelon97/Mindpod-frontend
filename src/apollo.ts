5aa27f30e247045619bb6ef21cfbdd06b01e0822
Soogeun Park <bwmelon97@naver.com>
2021-05-31 07:20:33 +0000

    Put token into header

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,12 +1,24 @@
-import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
-import { LOCALSTORAGE_TOKEN } from './constants';
+import { ApolloClient, createHttpLink, InMemoryCache, makeVar } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { GQL_ENDPOINT, LOCALSTORAGE_TOKEN } from '@constants';
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN)
 export const isLoggedInVar = makeVar(Boolean(token))
 export const authTokenVar = makeVar(token)
 
+const httpLink = createHttpLink({
+    uri: GQL_ENDPOINT
+})
+
+const authLink = setContext( (_, {headers}) => ({
+    headers: {
+        ...headers,
+        "x-jwt": authTokenVar() || ""
+    }
+}) )
+
 export const client = new ApolloClient({
-    uri: 'https://s-pod-backend.herokuapp.com/graphql',
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
     connectToDevTools: true
 });
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
+export const GQL_ENDPOINT = 'https://s-pod-backend.herokuapp.com/graphql'
+
 export const LOCALSTORAGE_TOKEN = "spod-token";
